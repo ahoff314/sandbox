f42838e9cd605915e25b3b9cf5698ea51846af98
ahoff314 <alexanderjhoffmann@gmail.com>
2017-01-14 13:58:26 -0700

    Data types

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
 int main() {
     std::cout << "This is my sandbox" << std::endl; // comment
+
+    const double PI = 3.1415926535;
+
+    char myGrade = 'A';
+
+    bool isHappy = true;
+
+    int floff = 24;
+
+    float favNum = 2.668568;
+
+    std::cout << "Favorite Number " << favNum << std::endl;
+
     return 0;
 }
